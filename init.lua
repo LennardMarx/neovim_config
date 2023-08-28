c4be3c216ffd92c6cfe552a16dd342c4fc51dc86
LennardMarx <l.marx@gmx.de>
2023-08-28 02:08:24 +0200

    Added LaTeX support

--- init.lua
+++ init.lua
@@ -27,4 +27,45 @@ require("astronvim.utils").conditional_func(astronvim.user_opts("polish", nil, f
 require "plugins.configs._gruvbox_config"
 require "user._keybindings"
 
-----------------------------------------------------------
+------------------ vim-plug ------------------------
+
+-- -- Configure vim-plug and install plugins
+-- vim.fn["plug#begin"](vim.fn.stdpath "data" .. "/plugged")
+--
+-- -- Add vimtex plugin
+-- vim.fn["plug#"] "lervag/vimtex"
+--
+-- -- You can add more plugins here
+-- vim.fn["plug#end"]()
+
+--------------------- VimTex config ----------------------------
+-- Set filetype-related settings
+vim.cmd "filetype plugin indent on"
+
+-- Enable syntax highlighting
+vim.cmd "syntax enable"
+
+-- Configure viewer options
+-- vim.g.vimtex_view_method = "mupdf" -- Built-in viewer method
+-- OR
+-- vim.g.vimtex_view_general_viewer = "okular" -- Generic interface
+-- vim.g.vimtex_view_general_viewer = "mupdf" -- Generic interface
+-- vim.g.vimtex_view_general_options = "--unique file:@pdf\\#src:@line@tex"
+vim.g.vimtex_view_general_viewer = "C:/Users/lmarx/AppData/Local/SumatraPDF/SumatraPDF.exe"
+vim.g.vimtex_view_general_options = "-reuse-instance -forward-search @tex @line @pdf"
+
+-- Configure the compiler backend
+-- vim.g.vimtex_compiler_method = "latexmk"
+
+-- Set localleader
+vim.g.maplocalleader = ","
+
+-- Install and configure plugins using vim-plug
+vim.fn["plug#begin"](vim.fn.stdpath "data" .. "/plugged")
+
+-- Add vimtex plugin
+vim.fn["plug#"] "lervag/vimtex"
+
+-- You can add more plugins here
+
+vim.fn["plug#end"]()
